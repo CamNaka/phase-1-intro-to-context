daee1c9e413b0833a5688ec1c5a62c983000278c
Cameron <cameronnakaba4@gmail.com>
2022-09-13 11:33:23 -0700

    commit tree dc78d6f16ce4e564f9c9b3d14cd5514d1a9daf93

--- index.js
+++ index.js
@@ -44,11 +44,11 @@ const createTimeOutEvent = function (dateStamp) {
   this.timeOutEvents.push(outEvent)
 
   return this
-}calculatePayroll
+}
 
 const hoursWorkedOnDate = function (targetDate) {
   const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
-  const outEvents = this.timeOutEvents = this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
+  const outEvents = this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
   return (outEvents.hour - inEvent.hour) / 100
 }
 
@@ -56,14 +56,35 @@ function wagesEarnedOnDate(targetDate) {
   return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
 }
 
+const allWagesFor = function () {
+  const eligibleDates = this.timeInEvents.map(function (e) {
+      return e.date
+  })
+
+  const payable = eligibleDates.reduce( (memo, d) => {
+      return memo + wagesEarnedOnDate.call(this, d)
+  }, 0) 
+
+  return payable
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(rec => rec.firstName === firstName)
+}
+
+function calculatePayroll(recsArray){
+   return recsArray.reduce((total, rec) => {
+      return total + allWagesFor.call(rec)
+   }, 0)
+}
 
 
 
 
 
 
-
-
+/*
 const allWagesFor = function(){
   const eligibaleDates = this.timeInEvents.map(function (e) {
     return e.date
@@ -75,3 +96,4 @@ const allWagesFor = function(){
    
   return payable
 }
+*/
