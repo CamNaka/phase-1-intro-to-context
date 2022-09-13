a562be7a6aef2e6a76e49e09dd1a8235c790a541
Cameron <cameronnakaba4@gmail.com>
2022-09-13 08:28:47 -0700

    intro to context

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-// Your code here
+
+
+
+
+
+function createEmployeeRecord(arrayEle) {
+  return {
+    firstName: arrayEle[0],
+    familyName: arrayEle[1],
+    title: arrayEle[2],
+    payPerHour: arrayEle[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  }
+}
+
+const createEmployeeRecords = (recordsArray) => {
+  return recordsArray.map(rec => createEmployeeRecord(rec))
+}
+
+const createTimeInEvent = function (dateStamp) {
+  const [date, hour] = dateStamp.split(" ")
+
+  const inEvent = {
+    type: "TimeIn",
+    hour: parseInt(hour),
+    date: date
+  }
+  this.timeInEvents.push(inEvent)
+
+  return this
+}
+
+
+
+const createTimeOutEvent = function (dateStamp) {
+  const [date, hour] = dateStamp.split(" ")
+
+  const outEvent = {
+    type: "TimeOut",
+    hour: parseInt(hour),
+    date: date
+  }
+  this.timeOutEvents.push(outEvent)
+
+  return this
+}calculatePayroll
+
+const hoursWorkedOnDate = function (targetDate) {
+  const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+  const outEvents = this.timeOutEvents = this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
+  return (outEvents.hour - inEvent.hour) / 100
+}
+
+function wagesEarnedOnDate(targetDate) {
+  return hoursWorkedOnDate.call(this, targetDate) * this.payPerHour
+}
+
+
+
+
+
+
+
+
+
+const allWagesFor = function(){
+  const eligibaleDates = this.timeInEvents.map(function (e) {
+    return e.date
+  })
+
+  const payable = eligibaleDates.reduce(function(memo, d) {
+    return memo + wagesEarnedOnDate.call(this, d)
+  }.blind(this), 0)
+   
+  return payable
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,5 @@
+const helpers = require('./helpers')
+
 describe("The payroll system", function () {
   describe("populates a record from an Array", function () {
     it("has a function called createEmployeeRecord", function () {
@@ -93,21 +95,21 @@ describe("The payroll system", function () {
 
       it("creates the correct type", function () {
         let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
-        let updatedBpRecord = createTimeInEvent(bpRecord, "2014-02-28 1400")
+        let updatedBpRecord = createTimeInEvent.call(bpRecord, "2014-02-28 1400")
         let newEvent = updatedBpRecord.timeInEvents[0]
         expect(newEvent.type).to.equal("TimeIn")
       })
 
       it("extracts the correct date", function () {
         let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
-        let updatedBpRecord = createTimeInEvent(bpRecord, "2014-02-28 1400")
+        let updatedBpRecord = createTimeInEvent.call(bpRecord, "2014-02-28 1400")
         let newEvent = updatedBpRecord.timeInEvents[0]
         expect(newEvent.date).to.eq("2014-02-28");
       })
 
       it("extracts the correct hour", function () {
         let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
-        let updatedBpRecord = createTimeInEvent(bpRecord, "2014-02-28 1400")
+        let updatedBpRecord = createTimeInEvent.call(bpRecord, "2014-02-28 1400")
         let newEvent = updatedBpRecord.timeInEvents[0]
         expect(newEvent.hour).to.eq(1400);
       })
@@ -125,21 +127,21 @@ describe("The payroll system", function () {
 
       it("creates the correct type", function () {
         let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
-        let updatedBpRecord = createTimeOutEvent(bpRecord, "2015-02-28 1700")
+        let updatedBpRecord = createTimeOutEvent.call(bpRecord, "2015-02-28 1700")
         let newEvent = updatedBpRecord.timeOutEvents[0]
         expect(newEvent.type).to.equal("TimeOut")
       })
 
       it("extracts the correct date", function () {
         let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
-        let updatedBpRecord = createTimeOutEvent(bpRecord, "2015-02-28 1700")
+        let updatedBpRecord = createTimeOutEvent.call(bpRecord, "2015-02-28 1700")
         let newEvent = updatedBpRecord.timeOutEvents[0]
         expect(newEvent.date).to.eq("2015-02-28");
       })
 
       it("extracts the correct hour", function () {
         let bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
-        let updatedBpRecord = createTimeOutEvent(bpRecord, "2015-02-28 1700")
+        let updatedBpRecord = createTimeOutEvent.call(bpRecord, "2015-02-28 1700")
         let newEvent = updatedBpRecord.timeOutEvents[0]
         expect(newEvent.hour).to.eq(1700);
       })
@@ -155,8 +157,8 @@ describe("The payroll system", function () {
     describe("hoursWorkedOnDate", function () {
       it("calculates that the employee worked 2 hours", function () {
         cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 1000])
-        updatedBpRecord = createTimeInEvent(cRecord, "0044-03-15 0900")
-        updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-15 1100")
+        updatedBpRecord = createTimeInEvent.call(cRecord, "0044-03-15 0900")
+        updatedBpRecord = createTimeOutEvent.call(cRecord, "0044-03-15 1100")
         expect(hoursWorkedOnDate(cRecord, "0044-03-15")).to.equal(2)
       })
     })
@@ -171,8 +173,8 @@ describe("The payroll system", function () {
     describe("wagesEarnedOnDate", function () {
       it("calculates that the employee earned 54 dollars", function () {
         cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27])
-        updatedBpRecord = createTimeInEvent(cRecord, "0044-03-15 0900")
-        updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-15 1100")
+        updatedBpRecord = createTimeInEvent.call(cRecord, "0044-03-15 0900")
+        updatedBpRecord = createTimeOutEvent.call(cRecord, "0044-03-15 1100")
         expect(wagesEarnedOnDate(cRecord, "0044-03-15")).to.equal(54)
       })
     })
@@ -188,11 +190,11 @@ describe("The payroll system", function () {
       it("calculates that the employee earned 378 dollars", function () {
         cRecord = createEmployeeRecord(["Julius", "Caesar", "General", 27])
         // Earns 324
-        updatedBpRecord = createTimeInEvent(cRecord, "0044-03-14 0900")
-        updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-14 2100")
+        updatedBpRecord = createTimeInEvent.call(cRecord, "0044-03-14 0900")
+        updatedBpRecord = createTimeOutEvent.call(cRecord, "0044-03-14 2100")
         // Earns 54
-        updatedBpRecord = createTimeInEvent(cRecord, "0044-03-15 0900")
-        updatedBpRecord = createTimeOutEvent(cRecord, "0044-03-15 1100")
+        updatedBpRecord = createTimeInEvent.call(cRecord, "0044-03-15 0900")
+        updatedBpRecord = createTimeOutEvent.call(cRecord, "0044-03-15 1100")
         // 324 + 54
         expect(allWagesFor(cRecord)).to.equal(378)
       })
@@ -221,14 +223,14 @@ describe("The payroll system", function () {
 
         sTimeData.forEach(function (d) {
           let [dIn, dOut] = d
-          sRecord = createTimeInEvent(sRecord, dIn)
-          sRecord = createTimeOutEvent(sRecord, dOut)
+          sRecord = createTimeInEvent.call(sRecord, dIn)
+          sRecord = createTimeOutEvent.call(sRecord, dOut)
         })
 
         rTimeData.forEach(function (d, i) {
           let [dIn, dOut] = d
-          rRecord = createTimeInEvent(rRecord, dIn)
-          rRecord = createTimeOutEvent(rRecord, dOut)
+          rRecord = createTimeInEvent.call(rRecord, dIn)
+          rRecord = createTimeOutEvent.call(rRecord, dOut)
         })
 
         let employees = [sRecord, rRecord]
